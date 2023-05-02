9bcfe878a67170587040850fe4f3cfce3eb8cf81
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-02 15:51:21 -0300

    Update and rename oracle to oracle.js

---
+++ src/dlc/oracle.js
@@ -0,0 +1,77 @@
+const crypto = require('crypto');
+
+class DLC {
+  constructor() {
+    this.oraclePrivateKey = null;
+    this.oraclePublicKey = null;
+    this.contractId = null;
+    this.fundingTxId = null;
+    this.fundingTxOutputIndex = null;
+    this.fundingAmount = null;
+    this.outcomeValue = null;
+    this.outcomeSignature = null;
+  }
+
+  generateKeys() {
+    const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 2048,
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem'
+      }
+    });
+    this.oraclePrivateKey = privateKey;
+    this.oraclePublicKey = publicKey;
+  }
+
+  createContractId() {
+    const hash = crypto.createHash('sha256');
+    hash.update(this.oraclePublicKey);
+    hash.update(this.fundingTxId);
+    hash.update(this.fundingTxOutputIndex.toString());
+    hash.update(this.fundingAmount.toString());
+    this.contractId = hash.digest('hex');
+  }
+
+  signOutcome(outcomeValue) {
+    const sign = crypto.createSign('sha256');
+    sign.update(outcomeValue);
+    sign.end();
+    this.outcomeSignature = sign.sign(this.oraclePrivateKey, 'hex');
+  }
+
+  verifyOutcome() {
+    const verify = crypto.createVerify('sha256');
+    verify.update(this.outcomeValue);
+    verify.end();
+    const isValid = verify.verify(this.oraclePublicKey, this.outcomeSignature, 'hex');
+    return isValid;
+  }
+
+  printContractDetails() {
+    console.log('Contract ID:', this.contractId);
+    console.log('Funding Tx ID:', this.fundingTxId);
+    console.log('Funding Tx Output Index:', this.fundingTxOutputIndex);
+    console.log('Funding Amount:', this.fundingAmount);
+    console.log('Outcome Value:', this.outcomeValue);
+    console.log('Outcome Signature:', this.outcomeSignature);
+  }
+}
+
+// Usage example:
+const dlc = new DLC();
+dlc.generateKeys();
+dlc.fundingTxId = 'abcd1234'; // Set the funding transaction ID
+dlc.fundingTxOutputIndex = 0; // Set the funding transaction output index
+dlc.fundingAmount = 100; // Set the funding amount
+dlc.createContractId();
+dlc.outcomeValue = 'Win'; // Set the outcome value
+dlc.signOutcome(dlc.outcomeValue);
+dlc.printContractDetails();
+const isValidOutcome = dlc.verifyOutcome();
+console.log('Is outcome valid?', isValidOutcome);
+

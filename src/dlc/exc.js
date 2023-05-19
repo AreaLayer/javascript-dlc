8ef5a427581691d1ca378a19170ed38c91dbde3f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-19 12:52:58 -0300

    Update exc.js

--- src/dlc/exc.js
+++ src/dlc/exc.js
@@ -1,1 +1,91 @@
+// Install BDK and LDK using npm or yarn
+const { ElectrumNetwork, Wallet, Psbt } = require('bdk');
+const { ChannelManager, PaymentStatus, Invoice } = require('ldk');
+
+// Set up BDK wallet
+const wallet = new Wallet('testnet', 'path/to/wallet.dat');
+
+// Connect to Electrum server
+const network = ElectrumNetwork.fromBitcoinAPI('https://electrum.example.com');
+wallet.addElectrumServer(network);
+
+// Set up LDK channel manager
+const channelManager = new ChannelManager('path/to/channel_manager.dat');
+
+// Connect to LDK node
+async function connectToNode() {
+  const nodeUri = '03abcdef...'; // Replace with the node URI
+  await channelManager.connect(nodeUri);
+}
+
+// Create an invoice for payment
+async function createInvoice(amount) {
+  const invoice = new Invoice({ amount });
+  await channelManager.waitForPersistentChannel();
+  const paymentResult = await channelManager.waitForPayment(invoice.paymentHash);
+  
+  if (paymentResult.status === PaymentStatus.SUCCEEDED) {
+    // Payment succeeded
+    console.log('Payment successful!');
+  } else {
+    // Payment failed
+    console.log('Payment failed!');
+  }
+}
+
+// Create and sign a funding transaction
+async function createFundingTransaction(address, value) {
+  const builder = wallet.createTxBuilder();
+  builder.addRecipients([{ address, value }]);
+
+  const psbt = await wallet.build(builder);
+  const signedPsbt = await wallet.sign(psbt);
+
+  return signedPsbt.extractTransaction().toHex();
+}
+
+// Create and sign the settlement transaction
+async function createSettlementTransaction(eventType, payoutAddress, payoutAmount) {
+  const builder = wallet.createTxBuilder();
+
+  // Use the event type in the covenant condition
+  const covenantScript = `OP_IF ${eventType} OP_ELSE OP_FALSE OP_ENDIF`;
+  builder.addOutput(payoutAmount, payoutAddress);
+  builder.addOutput(0, Buffer.from(covenantScript, 'hex'));
+
+  // Add other outputs, inputs, and fees as needed
+  // ...
+
+  const psbt = await wallet.build(builder);
+  const signedPsbt = await wallet.sign(psbt);
+
+  return signedPsbt.extractTransaction().toHex();
+}
+
+// Run the DLC
+async function runDLC() {
+  const fundingAddress = await wallet.getNewAddress();
+  const fundingValue = 100000; // Amount in satoshis
+
+  const settlementAddress = await wallet.getNewAddress();
+  const eventType = 'Team A wins'; // Replace with the desired event type
+  const payoutAddress = await wallet.getNewAddress();
+  const payoutAmount = 90000; // Amount in satoshis
+
+  const fundingTxHex = await createFundingTransaction(fundingAddress, fundingValue);
+  const settlementTxHex = await createSettlementTransaction(eventType, payoutAddress, payoutAmount);
+
+  await connectToNode();
+  await createInvoice(settlementValue);
+
+  // ... Rest of the DLC logic goes here ...
+
+  // Broadcast the settlement transaction
+  const psbt = Psbt.fromHex(settlementTxHex);
+  const signedPsbt = await wallet.sign(psbt);
+  await wallet.broadcast(signedPsbt.extractTransaction().toHex());
+}
+
+// Start the DLC
+runDLC().catch(console.error);
 

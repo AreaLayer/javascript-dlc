8491f752b2455713f8cca13bafdf973154922568
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-20 09:25:53 -0300

    Update ldk.js

--- src/lightning/ldk.js
+++ src/lightning/ldk.js
@@ -1,1 +1,74 @@
+const bitcoin = require('bitcoin-js');
+const ldk = require('ldk-node-js');
+const providerUrl = 'YOUR_PROVIDER_URL';
+const provider = new bitcoin(providerUrl);
+const ldkProvider = new ldk(providerUrl);
+
+class DLCContract {
+  constructor(address, api, provider) {
+    this.address = address;
+    this.api = api;
+    this.provider = provider;
+    this.contract = new this.provider.dlc.Contract(api, address);
+  }
+
+  async call(methodName, ...args) {
+    const method = this.contract.methods[methodName](...args);
+    const result = await method.call();
+    return result;
+  }
+
+  async sendTransaction(methodName, options, ...args) {
+    const method = this.contract.methods[methodName](...args);
+    const tx = {
+      from: options.from,
+      to: this.address,
+      fee: options.feesSats,
+      feePrice: options.feePrice,
+      value: options.value,
+      data: method.dataAPI()
+    };
+    const signedTx = await this.provider.accounts.signTransaction(tx, options.privateKey);
+    const result = await this.provider.sendSignedTransaction(signedTx.rawTransaction);
+    return result;
+  }
+}
+
+const address = 'YOUR_CONTRACT_ADDRESS';
+const api = [
+  {
+    "constant": true,
+    "inputs": [{ "name": "x", "type": "uint256" }],
+    "name": "multiply",
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+];
+const contract = new DLCContract(address, api, ldkProvider);
+
+async function testContract() {
+  const result = await contract.call('multiply', 7);
+  console.log(`Result: ${result}`);
+
+  const privateKey = 'YOUR_PRIVATE_KEY';
+  const options = {
+    from: 'YOUR_ADDRESS',
+    feesSats: 100000,
+    feePrice: provider.utils.toWei('10000000', 'sats'),
+    value: 0,
+    privateKey: privateKey
+  };
+  const txResult = await contract.sendTransaction('multiply', options, 5);
+  console.log(`Transaction hash: ${txResult.transactionHash}`);
+}
+
+testContract();
 

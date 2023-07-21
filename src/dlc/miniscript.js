8fa3165e94700ee9d7b66a9da2de6154c2eadd0f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-21 11:43:54 -0300

    Create miniscript.js

---
+++ src/dlc/miniscript.js
@@ -0,0 +1,50 @@
+// Example Miniscript for the Bet
+const MiniscriptBet = {
+  // Alice and Bob's public keys (assuming P2WSH for brevity)
+  alicePublicKey: "AlicePublicKey",
+  bobPublicKey: "BobPublicKey",
+
+  // Oracle public key (assuming P2WSH for brevity)
+  oraclePublicKey: "OraclePublicKey",
+
+  // Timeout for the contract (e.g., if the Oracle doesn't provide data in time)
+  timeoutBlocks: 100,
+
+  // Build the Miniscript for the Bet
+  buildScript: function(outcome) {
+    // Depending on the outcome, create the corresponding script branch
+    let branchScript;
+    if (outcome === 0) {
+      // Team A wins
+      branchScript = `
+        ${this.alicePublicKey} OP_CHECKSIG
+      `;
+    } else {
+      // Team B wins
+      branchScript = `
+        ${this.bobPublicKey} OP_CHECKSIG
+      `;
+    }
+
+    // Add the Oracle branch with timeout
+    const oracleBranch = `
+      ${this.oraclePublicKey} OP_CHECKSIGVERIFY
+      ${this.timeoutBlocks} OP_CSV OP_DROP
+    `;
+
+    // Combine all branches into the complete Miniscript
+    const fullScript = `
+      ${branchScript}
+      ${oracleBranch}
+    `;
+
+    return fullScript;
+  }
+};
+// Assuming the Oracle has provided the outcome: 0 for Team A wins, 1 for Team B wins
+const outcome = OracleScript.fetchData();
+
+// Alice and Bob will now construct their scripts using the outcome from the Oracle
+const aliceScript = MiniscriptBet.buildScript(outcome);
+const bobScript = MiniscriptBet.buildScript(outcome);
+

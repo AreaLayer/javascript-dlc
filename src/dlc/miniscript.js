f019404ef2383fac90c7ef6ead94a82140b123d1
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-25 13:31:58 -0300

    Update miniscript.js

--- src/dlc/miniscript.js
+++ src/dlc/miniscript.js
@@ -1,5 +1,11 @@
 const miniscript = require('miniscriptlib-js');
 
+
+
+// Mainnet or Testnet
+const NETWORK = bitcoin.networks.mainnet
+const NETWORK = bitcoin.networks.testnet
+
 const MiniscriptBet = {
   // Alice and Bob's public keys (assuming P2WSH for brevity)
   alicePublicKey: "AlicePublicKey",

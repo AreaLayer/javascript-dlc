fe2c47536c9c43933eb86c89531c0483f829d16c
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-19 06:25:49 -0300

    Update tapscript.js

--- src/dlc/tapscript.js
+++ src/dlc/tapscript.js
@@ -1,1 +1,24 @@
+const tapscriptModule = require('./tapscript/pkg/tapscript');
 
+const { Tapscript, parse_script } = tapscriptModule;
+
+async function run() {
+  // Initialize the WebAssembly module
+  await tapscriptModule;
+
+  const tapscript = new Tapscript();
+
+  try {
+    // Parse the Tapscript code
+    const parsedScript = parse_script(tapscriptCode);
+
+    // Interpret the parsed Tapscript
+    const result = tapscript.interpret(parsedScript);
+
+    console.log('Tapscript result:', result);
+  } catch (error) {
+    console.error('Error interpreting Tapscript:', error);
+  }
+}
+
+run();

d27679b8bfb4705df4a4312c7328cfd5c3e78036
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-10-30 05:54:00 -0300

    Update rpc.sh

--- rpc/rpc.sh
+++ rpc/rpc.sh
@@ -1,1 +1,8 @@
 ## rpc script
+
+  protocol: 'http',
+  user: 'your_rpc_username',
+  pass: 'your_rpc_password',
+  host: '127.0.0.1', // Replace with your Bitcoin Core's RPC server address
+  port: 8332,        // Replace with the RPC port you've configured
+};

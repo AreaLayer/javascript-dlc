e891bda36973eaaf77f9fb9d44a8205d8cb350a8
Rsync⚡️ <135646455+Rsync25@users.noreply.github.com>
2023-12-21 08:30:54 -0300

    Update miniscript.js

--- src/dlc/miniscript.js
+++ src/dlc/miniscript.js
@@ -1,7 +1,7 @@
 const miniscript = require('miniscriptlib-js');
 
 // Mainnet or Testnet
-const NETWORK = bitcoin.networks.mainnet
+const NETWORK = bitcoin.networks.bitcoin;
 
 const MiniscriptBet = {
   // Alice and Bob's public keys (assuming P2WSH for brevity)

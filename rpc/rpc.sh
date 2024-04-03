59d543df02c379e96f864cb1552fef2911625847
Rsync⚡️ <135646455+Rsync25@users.noreply.github.com>
2024-04-03 06:40:07 -0300

    Update rpc.sh

--- rpc/rpc.sh
+++ rpc/rpc.sh
@@ -5,4 +5,5 @@
   pass: 'your_rpc_password',
   host: '127.0.0.1', // Replace with your Bitcoin Core's RPC server address
   port: 8332,        // Replace with the RPC port you've configured
+  tor: 
 };

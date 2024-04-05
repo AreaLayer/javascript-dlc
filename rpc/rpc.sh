901a43d62b950994fbe16bb6a39b29e1cb1c2422
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-04-05 05:35:51 -0300

    Update rpc.sh

--- rpc/rpc.sh
+++ rpc/rpc.sh
@@ -3,7 +3,7 @@
   protocol: 'http',
   user: 'your_rpc_username',
   pass: 'your_rpc_password',
-  host: '127.0.0.1', // Replace with your Bitcoin Core's RPC server address
-  port: 8332,        // Replace with the RPC port you've configured
-  tor: 
+  host: '127.0.0.1', // Replace with your   Bitcoin Core's RPC server address
+  port: 8332, // Replace with the RPC port you've configured
+  tor: address, 
 };

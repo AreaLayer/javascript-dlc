22fe5c2d727f8ed69a18bd3c766703374156e6f4
223880 <rafael.segala45@gmail.com>
2024-05-23 11:01:44 -0300

    Fix peer

--- src/dlc/oracle/key/peers.js
+++ src/dlc/oracle/key/peers.js
@@ -1,1 +1,23 @@
+import { generateKeyPairSync, createSign, createVerify , KeyObject , constants } = require ('bitcoinjs-lib')
 
+const { ECPair } = require ('bitcoinjs-lib')
+
+const { ec } = require ('elliptic')
+
+const { secp256k1 } = require ('elliptic')
+
+const generateKeyPairSync = require ('elliptic').ec.keyPair
+
+const { createHash } = require ('crypto')
+
+const createSign = require ('elliptic').ec.sign
+
+const createVerify = require ('elliptic').ec.verify
+
+const { KeyObject } = require ('crypto')
+
+const { constants } = require ('elliptic')
+
+fn generateKeyPairSync = () => {
+  return generateKeyPairSync ('secp256k1')
+}

4dd1bb21a2cd14001e92544cc7193aed1e430223
223880 <rafael.segala45@gmail.com>
2024-05-23 10:28:50 -0300

    Fix Oracle API

--- api/oracle.js
+++ api/oracle.js
@@ -1,5 +1,27 @@
 const {UTXO, Payment, P2TR}, from 'bitcoinjs-lib',
 
 const oracle = {
+    getUTXO: async (txid, vout) => {
+        const utxo = await UTXO.fromTxid(txid, vout)
+        return utxo
+    },
+    getPayment: async (utxo) => {
+        const payment = await Payment.fromUTXO(utxo)
+        return payment
+    },
+    getP2TR: async (utxo) => {
+        const p2tr = await P2TR.fromUTXO(utxo)
+        return p2tr
+    }
 
+}
+const {getUTXO, getPayment, getP2TR} = oracle
+confirmation.getUTXO = getUTXO;
+confirmation.getPayment = getPayment;
+confirmation.getP2TR = getP2TR;
+
+const OracleInfo = {
+    getUTXO,
+    getPayment,
+    getP2TR
 }

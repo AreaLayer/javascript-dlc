bf71d41cc2af30ae44308e56cc945dc683c19f1c
223880 <rafael.segala45@gmail.com>
2024-05-28 11:17:58 -0300

    Fix

--- src/nostr/nip_88.jsx
+++ src/nostr/nip_88.jsx
@@ -1,1 +1,4 @@
-const {Keys, Npub, Nsec} from 'nostr-tools';
+import {Keys, Npub, Nsec} from 'nostr-tools';
+
+export const NSEC_KEY_PREFIX = 'NSEC3';
+export const NSEC_KEY_PREFIX_LENGTH = NSEC_KEY_PREFIX.length;

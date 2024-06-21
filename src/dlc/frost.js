394b243fc7a71bed3ea7941eb317ff88c1ffae7f
223880 <rafael.segala45@gmail.com>
2024-06-21 07:50:54 -0300

    Initial test with JS Frost and fixed json

--- src/dlc/frost.js
+++ src/dlc/frost.js
@@ -1,1 +1,17 @@
+const { Keygen } = require ('@javascript-frost/keygen');
 
+module.exports = Keygen;
+module.exports.Keygen = Keygen;
+
+const { KeygenError } = require ('./error');
+module.exports.KeygenError = KeygenError;
+
+const { KeygenError_InvalidKey } = require ('./error_invalid_key');
+module.exports.KeygenError_InvalidKey = KeygenError_InvalidKey;
+
+const Keygen = {
+    Keygen,
+    KeygenError,
+    KeygenError_InvalidKey
+
+}

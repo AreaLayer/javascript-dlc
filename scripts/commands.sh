1f9d6ac8957ea332bc262d0d40198f29a573236d
223880 <rafael.segala45@gmail.com>
2024-07-29 09:41:37 -0300

    Fix

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -6,14 +6,16 @@ OP_IF OP_TRUE OP_ELSE OP_FALSE OP_ENDIF
 
 OP_IF ${eventType} OP_ELSE OP_FALSE OP_ENDIF
 
+OP_IF <condition> OP_ELSE OP_FALSE OP_ENDIF
+
 # ECDCSA script
 
-OP_IF ${eventType} OP_ELSE OP_FALSE OP_ENDIF
+OP_IF <condition> OP_ELSE OP_FALSE OP_ENDIF
 
 # curve ed25519 script
 
-OP_IF ${eventType} OP_ELSE OP_FALSE OP_ENDIF
+OP_IF <condition> OP_ELSE OP_FALSE OP_ENDIF
 
 # curve secp256k1 script
 
-OP_IF ${eventType} OP_ELSE OP_FALSE OP_ENDIF
+OP_IF <condition> OP_ELSE OP_FALSE OP_ENDIF

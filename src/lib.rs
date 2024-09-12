f937eac7eac6375c1f405b7fb503331ae5878da3
223880 <rafael.segala45@gmail.com>
2024-09-12 06:40:24 -0300

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 // Import everything necessary from wasm_bindgen
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
 
 // Import the console logging functionality from the web_sys crate
 use web_sys::console;
@@ -12,9 +11,5 @@ pub fn greet(name: &str) -> String {
 
 #[wasm_bindgen]
 pub fn log(msg: &str) {
-    console::log_1(&msg.into());
-}
-#[wasm_bindgen]
-pub fn JsCast(msg: &str) {
     console::log_1(&msg.into());
 }

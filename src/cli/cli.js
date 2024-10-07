37103fb5750f89d00e4c31b92da218e43066ab11
223880 <rafael.segala45@gmail.com>
2024-10-07 06:50:13 -0300

    Fix CLI commands

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -6,7 +6,11 @@ const { lightning_channel } = require('./lightning_channel');
 program
   .version('1.0.0')
   .description('A CLI for Javascript')
-  .option('-n, --ligthnin <option', 'Lightning Channel')
+  .option('-n, --ligthning <option', 'Lightning Channel')
+  .option('-n, --dlc <option>', 'DLC')
+  .option( '-n  --cancel <option>', 'Transition Cancel')
+  .option( '-n  --fund <option>', 'Transition Fund')
+  .option( '-n -oracle <option>', 'Transition Oracle')
   .action((options) => {
     console.log('Lightning Channel:', options.name);
     myLibraryFunction(options.lightning_channel); // Call the library function

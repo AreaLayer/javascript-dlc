cb27d4c38e46c14d4e2dc301727214c2de1df4b9
22388o <22388o@users.noreply.github.com>
2024-11-30 08:05:06 -0300

    Fix lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,21 @@ use wasm_bindgen::prelude::*;
 // Import the console logging functionality from the web_sys crate
 use web_sys::console;
 
+
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
-    format!("DLC, {}!", name)
+    // Ensure the string is formatted correctly.
+    format!("Hello, {}!", name)
 }
 
 #[wasm_bindgen]
 pub fn log(msg: &str) {
+    // Safely log the message to the browser's console.
     console::log_1(&msg.into());
 }
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
+    // Return the sum of the two integers.
     a + b
 }

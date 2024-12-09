5a1d03707620416e61acc39bf2f47735eedb16d3
22388o <22388o@users.noreply.github.com>
2024-12-09 06:19:47 -0300

    Fix RPC

--- rpc/rpc.sh
+++ rpc/rpc.sh
@@ -6,4 +6,5 @@
   host: '127.0.0.1', // Replace with your   Bitcoin Core's RPC server address
   port: 8332, // Replace with the RPC port you've configured
   tor: 'onion' // Replace with the Tor address if you're using Tor
+  i2p: 'onion' // Replace with the I2P address if you're using I2P
 };

a8208f8a0496c4549bdf372ed8c0c8a5d315517b
223880⚡️ <83122757+22388o@users.noreply.github.com>
2025-07-29 08:19:26 -0300

    Update nip_88.jsx

--- src/nostr/nip_88.jsx
+++ src/nostr/nip_88.jsx
@@ -8,13 +8,5 @@ const NSEC_KEY_LENGTH_BYTES = NSEC_KEY_LENGTH / 8;
 const Npub_LENGTH = 32;
 
 const relays = {
-    'wss://relay.damus.io': 'wss://relay.damus.io',
-    'wss://relay.nostr.info': 'wss://relay.nostr.info',
-    'wss://nostr.bitcoiner.social': 'wss://nostr.bitcoiner.social',
-    'wss://nostr.fmt.wiz.biz': 'wss://nostr.fmt.wiz.biz',
-    'wss://nostr.mom': 'wss://nostr.mom',
-    'wss://nostr.mom.cat': 'wss://nostr.mom.cat',
-    'wss://nostr.mom.cat': 'wss://nostr.mom.cat',
-    'wss://nostr.mom.cat': 'wss://nostr.mom.cat',
-    'wss://nostr.mom.cat': 'wss://nostr.mom.cat',
+    'wss://https://relay.nostr.net': 'wss://https://relay.nostr.net'
 }
